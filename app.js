7549a561dcaf2b6e8f2ed751537828af2b1a8b64
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2021-06-13 19:20:36 -0500

    Request listo

--- app.js
+++ app.js
@@ -1,43 +1,28 @@
-const xmlhttp = new XMLHttpRequest();
+const API = 'https://rickandmortyapi.com/api/character/';
+const info_container = document.querySelector('.cards-container')
 
-function fetchData(URL_API, callback){
-    xmlhttp.open('GET', URL_API, true)
-    xmlhttp.onreadystatechange = (event) => {
-        if(xmlhttp.readyState === 4){
-            if(xmlhttp.status === 200){
-                callback(null, JSON.parse(xmlhttp.responseText))                
-            }
-            else {
-                const error = new Error(`Error ${URL_API}`)
-                return callback(error, null)
-            }           
-        }
+const mostrarInfo = (personaje) => {
+    return (
+        `<article>
+        <img src="${personaje.image}">
+        <p>${personaje.name}</p>`
+    )
+}
+const getData = async () => {
+    try {
+        const data = await fetch(API)
+        const data2 = await fetch(`${API}?page=2`)
+        const resp = await data.json()
+        const resp2 = await data2.json()
+        const characters = await resp.results
+        const characters2 = await resp2.results
+        let charachter = await characters.map(mostrarInfo)
+        let character2 = await characters2.map(mostrarInfo)
+        info_container.innerHTML = charachter.join('\n') + character2.join('\n')
+    } 
+    catch {
+        const error = new Error(`Error ${API}`)
+        console.log(error)
     }
-    xmlhttp.send()
 }
-
-let str = 'AAAABBBCCDAABBB';
-function transformar(iterable){
-    let arr = iterable.split('')
-    console.log(arr)
-    let arreglo = arr.map(()=> {
-        
-    })
-}
-transformar(str)
-
-let iterable = 'AAAABBBCCDAABBB'
-
-var uniqueInOrder = function(iterable){
-  let arr = []
-  for(var i; i < iterable.length; i++){
-    let valor_actual = iterable[i];
-    let siguiente_valor = iterable[i + 1];
-    
-    if(valor_actual !== siguiente_valor){
-      arr.push(valor_actual)
-    }
-  }
-  return arr
-}
-uniqueInOrder(iterable)
+getData()
