ae038fb3e12ae50528bec7ace22729f6e6e88c74
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2021-06-14 09:04:10 -0500

    Audio y estilos terminados

--- app.js
+++ app.js
@@ -1,13 +1,75 @@
 const API = 'https://rickandmortyapi.com/api/character/';
 const info_container = document.querySelector('.cards-container')
+const audio = document.querySelector('#audio');
+const volume_on = document.querySelector('#volume-on');
+const volume_off = document.querySelector('#volume-off');
+const volume_container = document.querySelector('.audio-container')
 
+audio.volume = 1;
+
+volume_off.addEventListener('click', playPause) 
+volume_on.addEventListener('click', playPause)
+
+function playPause(){
+    if(audio.paused){
+        audio.play()
+        volume_off.classList.add('hide')
+        volume_on.classList.add('show')
+    }
+    else {
+        audio.pause()
+        volume_off.classList.remove('hide')
+        volume_on.classList.remove('show')
+        volume_on.classList.add('hide')
+    }
+}
+
+
+/* volume_off.addEventListener('click', function(){
+    if(audio.paused){
+        audio.play()
+        volume_off.classList.add('hide')
+    }
+    else {
+        audio.pause()
+        volume_on.classList.add('hide')
+        volume_off.classList.add('show')
+    }
+}) */
 const mostrarInfo = (personaje) => {
     return (
         `<article>
         <img src="${personaje.image}">
-        <p>${personaje.name}</p>`
+        <h3>${personaje.name}</h3>
+        <p>${personaje.species}</p>
+        <p>${personaje.location.name}</p>
+        </article>`
     )
 }
+
+const getData = (url_api) => {
+    return fetch(url_api)
+        .then(response => response.json())
+}
+
+const characterData = async (url_api) => {
+    try {
+        const alldata = await getData(url_api);
+    /* const data_page2 = await getData(`${url_api}?page=2`); */
+    console.log(alldata)
+    const characters = await alldata.results
+    console.log(characters)
+    let character = characters.map(mostrarInfo)
+    info_container.innerHTML = character.join('\n')    
+    } 
+    catch {
+        const error = new Error(`Error ${API}`)
+        console.log(error)
+    }
+}
+
+characterData(API)
+/* 
 const getData = async () => {
     try {
         const data = await fetch(API)
@@ -26,3 +88,4 @@ const getData = async () => {
     }
 }
 getData()
+ */
